1bf9351f0f4d13c61f4807b43797d93ca575533d
Stefan Richter <jdevsr3@googlemail.com>
2016-02-14 15:58:21 +0100

    Initial commit

---
+++ app/src/main/java/io/github/sealor/prototype/android/espresso/ui/testing/MainActivity.java
@@ -0,0 +1,50 @@
+package io.github.sealor.prototype.android.espresso.ui.testing;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText input;
+    private TextView output;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.input = (EditText) findViewById(R.id.input);
+        this.output = (TextView) findViewById(R.id.output);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                output.setText(String.format("Hello %s!", input.getText()));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/test/java/io/github/sealor/prototype/android/espresso/ui/testing/JavaUnitTest.java
@@ -0,0 +1,13 @@
+package io.github.sealor.prototype.android.espresso.ui.testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JavaUnitTest {
+
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
